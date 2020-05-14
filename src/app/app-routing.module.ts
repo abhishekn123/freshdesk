7a62671f33849ff03dfc1dd734b8089490c9ea6b
abhishekn123 <abhiabhishekn164@gmail.com>
2020-05-14 13:50:19 +0530

    latest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ContactComponent } from "./Contacts/contact/contact.component";
+import { TicketComponent } from "./Ticket/ticket/ticket.component";
+const routes: Routes = [
+  {
+    path: "contacts",
+    component: ContactComponent,
+  },
+  {
+    path: "tickets",
+    component: TicketComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
